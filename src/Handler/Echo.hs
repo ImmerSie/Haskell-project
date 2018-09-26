2027d09ed0bee715255fbf43e91c3e2f40ccdca8
ImmerSie <31569454+ImmerSie@users.noreply.github.com>
2018-09-27 00:04:07 +1000

    Get echo of url in page

---
+++ src/Handler/Echo.hs
@@ -0,0 +1,10 @@
+{-# LANGUAGE QuasiQuotes #-}
+
+module Handler.Echo where
+
+import Import
+
+getEchoR :: String -> Handler RepHtml
+getEchoR theText = do
+    defaultLayout $ do
+        [whamlet|<h1>#{theText}|]
